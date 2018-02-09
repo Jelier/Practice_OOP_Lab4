9a208b65f7bf8063b313dfe65869e7b1c0ae4301
Jelllier <Jelllier@mail.ru>
2018-02-09 18:10:42 +0700

    First Shapes commit

---
+++ Shapes/Shapes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hexagon hex = new Hexagon("Beth");
+            hex.Draw();
+            Circle cir = new Circle("Cindy");
+            cir.Draw();
+            Console.ReadLine();
+        }
+    }
+}
