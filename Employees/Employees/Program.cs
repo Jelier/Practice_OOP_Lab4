aace1f839001d29ab968cea075cc102df0caf2a7
Jelllier <Jelllier@mail.ru>
2018-02-09 18:09:51 +0700

    backub commit

---
+++ Employees/Employees/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager chucky = new Manager("Chucky", 50, 92, 100000, "333-23-2322", 9000);
+            chucky.GiveBonus(300);
+            chucky.DisplayStats();
+
+            Salesperson fran = new Salesperson("Fran", 43, 93, 3000, "932-23-2322", 31);
+            fran.GiveBonus(200);
+            fran.DisplayStats();
+            Console.ReadLine();
+        }
+    }
+}
